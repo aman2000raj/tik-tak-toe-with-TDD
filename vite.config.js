ebfd1a106a625f88400c0c22d61daabf7bf024a6
aman2000raj <amankayshap10@gmail.com>
2024-05-04 23:38:43 +0530

    Initialize project using vite with testing library and jest.

---
+++ vite.config.js
@@ -0,0 +1,35 @@
+import { defineConfig, transformWithEsbuild } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [
+    {
+      name: 'treat-js-files-as-jsx',
+      async transform(code, id) {
+        if (!id.match(/src\/.*\.js$/)) return null;
+
+        // Use the exposed transform from vite, instead of directly
+        // transforming with esbuild
+        return transformWithEsbuild(code, id, {
+          loader: 'jsx',
+          jsx: 'automatic',
+        });
+      },
+    },
+    react(),
+  ],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: 'setup.js',
+  },
+
+  optimizeDeps: {
+    force: true,
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
+});
