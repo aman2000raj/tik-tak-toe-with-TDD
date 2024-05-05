795f34b04cd7eff222bfc69bd14d477cec5c917a
aman2000raj <amankayshap10@gmail.com>
2024-05-05 13:28:26 +0530

    Add onSquareClick prop to handle change when clicked.

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Square({ value, onSquareClick }) {
-  return <div className='square' onClick={onSquareClick}>{value  ?? 0}</div>;
+  return (
+    <div className='square' onClick={onSquareClick}>
+      {value ?? 0}
+    </div>
+  );
 }
 
 export default Square;
