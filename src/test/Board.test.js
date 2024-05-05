38c383e88687f3990e5b808f343f2876d6717b11
aman2000raj <amankayshap10@gmail.com>
2024-05-05 14:39:21 +0530

    Add test case(should call onPlay when a square is clicked) to Board.

--- src/test/Board.test.js
+++ src/test/Board.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Board from '../components/Board';
 
 describe('Board Compoent', () => {
@@ -10,7 +10,15 @@ describe('Board Compoent', () => {
     expect(screen.getAllByRole('button', { name: '' })).toHaveLength(9);
   });
 
-  it('should call onPlay when a square is clicked', async () => {});
+  it('should call onPlay when a square is clicked', async () => {
+    const mockOnPlay = vi.fn();
+    const squares = Array(9).fill(null);
+    render(<Board squares={squares} onPlay={mockOnPlay} />);
+
+    fireEvent.click(screen.getAllByRole('button', { name: '' })[0]);
+
+    expect(mockOnPlay).toHaveBeenCalledTimes(1);
+  });
 
   it('should display the winner when a row is filled', () => {});
 });
