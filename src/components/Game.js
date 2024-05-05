f2015007767450d8a4205e7bb8940a26c5165643
aman2000raj <amankayshap10@gmail.com>
2024-05-05 23:01:30 +0530

    Update Game component with handleClick method and player's turn logic.

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,11 +3,17 @@ import Board from './Board';
 
 function Game() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  function handleClick(nextSquares) {
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
   return (
     <>
       <div className='game'>
         <div className='game-board'>
-          <Board squares={squares} />
+          <Board xIsNext={xIsNext} squares={squares} onPlay={handleClick} />
         </div>
       </div>
     </>
