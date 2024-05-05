58699040ea3c610e6e0fa44e13d56ec69448a5c9
aman2000raj <amankayshap10@gmail.com>
2024-05-05 12:53:53 +0530

    Add some test suits.

---
+++ src/test/Board.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Board from './Board';
+
+describe('Board Compoent', () => {
+  it('should render the board with squares', () => {});
+
+  it('should call onPlay when a square is clicked', async () => {});
+
+  it('should display the winner when a row is filled', () => {});
+});
